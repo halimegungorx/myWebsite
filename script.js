d3b6e2d284edf552997cf75b77a6b1ffc18b862a
Halime Güngör <55948947+halimegungorx@users.noreply.github.com>
2022-08-19 18:57:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let menu = document.getElementsByClassName("#nav-link");
+let cubuk = document.getElementsByClassName("#nav-item");
+
+
+menu.addEventListener("click",function(){
+    console.log("butona tıklandı");
+})
